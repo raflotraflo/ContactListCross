f685ed68257b53c6a2d0dd05fb27d355a66988aa
Rafał Chodzidło <raflotraflo@gmail.com>
2017-01-23 20:47:45 +0100

    Finished the lesson 6.5

--- Core/Models/Contact.cs
+++ Core/Models/Contact.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Services;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +22,38 @@ namespace Core.Models
             PhoneNumber = phoneNumber;
         }
 
+        private IMvxCommand _emailContact;
+        public IMvxCommand EmailContact
+        {
+            get
+            {
+                if (_emailContact == null)
+                {
+                    _emailContact = new MvxCommand(() =>
+                    {
+                        Mvx.Resolve<IEmailService>().OpenClient(Email);
+                    });
+                }
 
+                return _emailContact;
+            }
+        }
+
+        private IMvxCommand _callContact;
+        public IMvxCommand CallContact
+        {
+            get
+            {
+                if (_callContact == null)
+                {
+                    _callContact = new MvxCommand(() =>
+                    {
+                        Mvx.Resolve<ICallerService>().Call(PhoneNumber);
+                    });
+                }
+
+                return _callContact;
+            }
+        }
     }
 }
