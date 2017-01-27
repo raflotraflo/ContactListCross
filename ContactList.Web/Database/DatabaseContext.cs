08cc84ce316545bafb384cdcb829b384c1ea8745
Rafał Chodzidło <raflotraflo@gmail.com>
2017-01-27 01:04:49 +0100

    Addition online database and publication service on Azure

--- ContactList.Web/Database/DatabaseContext.cs
+++ ContactList.Web/Database/DatabaseContext.cs
@@ -11,7 +11,7 @@ namespace ContactList.Web.Database
     {
         public DbSet<ContactEntity> Contacts { get; set; }
 
-        public DatabaseContext() : base()
+        public DatabaseContext() : base("Server=tcp:contactlist.database.windows.net,1433;Initial Catalog=ContactListDb;Persist Security Info=False;User ID=contactlist;Password=Krzysztof90;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
         {
 
         }
